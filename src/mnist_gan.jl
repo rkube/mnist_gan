106c6f1e039e99d353b2b15de644261d5bf903bc
Ralph Kube <ralph_kube@gmx.net>
2021-10-14 08:28:11 -0400

    Training loop is working but bug in generator training

--- src/mnist_gan.jl
+++ src/mnist_gan.jl
@@ -13,7 +13,7 @@ batch_size=128
 n_features = 28*28
 n_gen = 100
 num_epochs = 1
-η = 1e-3
+η = 2e-4
 
 # Load MNIST train and test data
 train_x, train_y = MNIST.traindata(Float32)
@@ -73,8 +73,73 @@ generator = Chain(Dense(n_gen, 256, leakyrelu),
 #       ∇_theta_g 1/m Σ_{i=1}^{m} log(1 - D(G(zⁱ))
 #   end for
 
+# Optimizer for the discriminator
+opt_dscr = ADAM(η)
+opt_gen = ADAM(η)
+
+function train_dscr(discriminator, real_data, fake_data)
+    # Given real and fake data, update the parameters of the discriminator network
+    # Assume that size(real_data) = 784xBS
+    # Feed real data to the discriminator. For these, the discriminator should return ones.
+    pred_real = discriminator(real_data)
+    grads_real = gradient(Flux.params(discriminator)) do
+        loss_real = Flux.Losses.logitbinarycrossentropy(pred_real, ones(size(pred_real)))
+    end
+    loss_real = Flux.Losses.logitbinarycrossentropy(pred_real, ones(size(pred_real)))
+    
+
+    # Train on fake data
+    # Generate fake data and feed it to the discrminiator. For these, the discriminator should return zeros.
+    pred_fake = discriminator(fake_data)
+    grads_fake = gradient(Flux.params(discriminator)) do
+        loss_fake = Flux.Losses.logitbinarycrossentropy(pred_fake, zeros(size(pred_fake)))
+    end
+    loss_fake = Flux.Losses.logitbinarycrossentropy(pred_fake, zeros(size(pred_fake)))
+
+    # Update the parameters of the discriminator with the respective gradients.
+    # This happens in-place(! of function signature)
+    Flux.update!(opt_dscr, Flux.params(discriminator), grads_real)
+    Flux.update!(opt_dscr, Flux.params(discriminator), grads_fake)
+
+    return loss_real + loss_fake, pred_real, pred_fake
+end
+
+
+function train_gen(discriminator, generator)
+    # Let the generator create fake data which should out-smart the discriminator
+    # Let's see how well the discriminator does
+    grads = gradient(Flux.params(generator)) do
+        fake_data = generator(randn(n_gen, batch_size))
+        pred = discriminator(fake_data)
+        loss = Flux.Losses.logitbinarycrossentropy(pred, ones(size(pred)))
+    end
+    @show grads
+    Flux.update!(opt_gen, Flux.params(generator), grads)
+    # Return the loss
+    loss = Flux.Losses.logitbinarycrossentropy(pred, ones(size(pred)))
+end
+
+
+for n in 1:num_epochs
+    for (x, y) in train_loader
+        # Train the discriminator
+        # First flatten the images, which squashes all dimensions and keeps the 
+        # the last dimension, which is the batch dimension
+        x = flatten(x) 
+
+        # Now generator fake data
+        fake_data = generator(randn(n_gen, batch_size))
+
+        # And train the discriminator
+        loss_dscr, pred_real, pred_fake = train_dscr(discriminator, x, fake_data)
+
+        # Train the generator
+        loss_gen = train_gen(discriminator, generator)
+
+        @show loss_dscr, loss_gen, pred_real, pred_fake
+    end
+end
 
 
 
 
-end # module
